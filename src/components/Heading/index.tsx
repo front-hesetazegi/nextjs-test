e325f7d6337ee5de306b36973d6ff5130b86b2c3
front-hesetazegi <fronthesetazegi@gmail.com>
2025-01-05 15:52:15 +0330

    add heading component

---
+++ src/components/Heading/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+// Define the possible variants for the heading
+type HeadingProps = {
+  label: string;
+  variant: "green" | "dark" | "white";
+};
+
+// Heading component
+const Heading: React.FC<HeadingProps> = ({ label, variant }) => {
+  const variantClasses: Record<string, string> = {
+    green: "text-black bg-green",
+    dark: "text-white bg-dark",
+    white: "text-black bg-white ",
+  };
+
+  const headingClass = variantClasses[variant] || "text-black";
+
+  return (
+    <h2 className={`font-medium text-[2.5rem] px-2 ${headingClass}`}>
+      {label}
+    </h2>
+  );
+};
+
+export default Heading;
